e0c15c0268cb3bc4faff06086ad0bce4ece0b863
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-08-29 14:54:51 +0800

    mod: stop animation when the tree is fully generated

--- webgl-tree/tree.js
+++ webgl-tree/tree.js
@@ -25,32 +25,38 @@ var renderer, scene, camera, stats,
     MAX_SPREAD_RADIAN = PI / 4,
     MIN_SPREAD_RADIAN = PI / 10,
 
-    BASE_LEAF_SCALE = 5;
+    BASE_LEAF_SCALE = 5,
+
+    branch_counter;
 
     
 
 
 function init() {
 
-    // setup track-ball camera
-    camera = new THREE.TrackballCamera({
-        fov: 45,
-        aspect: window.innerWidth / window.innerHeight,
-        near: 1,
-        far: 5000,
+    ////setup track-ball camera
+    //camera = new THREE.TrackballCamera({
+        //fov: 45,
+        //aspect: window.innerWidth / window.innerHeight,
+        //near: 1,
+        //far: 5000,
 
-        rotateSpeed: 1.0,
-        zoomSpeed: 1.2,
-        panSpeed: 0.8,
+        //rotateSpeed: 1.0,
+        //zoomSpeed: 1.2,
+        //panSpeed: 0.8,
 
-        noZoom: false,
-        noPan: false,
+        //noZoom: false,
+        //noPan: false,
 
-        staticMoving: true,
-        dynamicDampingFactor: 0.3,
+        //staticMoving: true,
+        //dynamicDampingFactor: 0.3,
 
-        keys: [65, 83, 68] // rotate, zoom, pan
-    });
+        //keys: [65, 83, 68] rotate, zoom, pan
+    //});
+    camera = new THREE.Camera(45,
+                              SCREEN_WIDTH / SCREEN_HEIGHT,
+                              1,
+                              5000);
     camera.position.set(500, 100, 400);
     camera.translate(200, new THREE.Vector3(0, 1, 0));
 
@@ -84,22 +90,23 @@ function init() {
     document.body.appendChild(container);
     container.appendChild(renderer.domElement);
 
-    // setup stats
-    stats = new Stats();
-    stats.domElement.style.position = "absolute";
-    stats.domElement.style.top = "0px";
-    stats.domElement.style.zIndex = 100;
-    container.appendChild(stats.domElement);
+    ////setup stats
+    //stats = new Stats();
+    //stats.domElement.style.position = "absolute";
+    //stats.domElement.style.top = "0px";
+    //stats.domElement.style.zIndex = 100;
+    //container.appendChild(stats.domElement);
 }
 
 
 function drawTree(start_position, direction, length, depth, radius) {
 
-
     var cylinder, half_length_offset,
         new_position, new_direction, new_length, new_depth, new_radius,
         new_base_position, offset_vector,
         num_branches, color, num_segs;
+
+    branch_counter--;
 
     // determine branch color
     if (depth < 3) {
@@ -130,18 +137,14 @@ function drawTree(start_position, direction, length, depth, radius) {
     cylinder.position.addSelf(half_length_offset);
 
     cylinder.castShadow = true;
-    cylinder.receiveShadow = true;
+    cylinder.receiveShadow = false;
 
     scene.addObject(cylinder);
 
     // stop recursion if depth reached 1
     if (depth == 1) {
-        //drawLeaf(start_position,
-                 //direction,
-                 //BASE_LEAF_SCALE * radius);
         return;
     }
-
 
 
     // calculate the base start position for next branch
@@ -157,63 +160,49 @@ function drawTree(start_position, direction, length, depth, radius) {
     num_branches = round((rand() * (MAX_BRANCHES - MIN_BRANCHES))) 
                    + MIN_BRANCHES;
 
+
+    // recursively draw the children branches
     for (var i = 0; i < num_branches; ++i) {
+
+        // random spread radian
         var spread_radian = rand() * (MAX_SPREAD_RADIAN - MIN_SPREAD_RADIAN) + 
                             MIN_SPREAD_RADIAN;
 
+        // generate a vector which is prependicular to the original direction
         var perp_vec = (new THREE.Vector3(1, 0, 0)).crossSelf(direction); 
         perp_vec.setLength(direction.length() * tan(spread_radian));
 
-
+        // the new direction equals to the sum of the perpendicular vector
+        // and the original direction
         new_direction = direction.clone().addSelf(perp_vec).normalize();
 
+        // generate a rotation matrix to rotate the new direction with
+        // the original direction being the rotation axis
         var rot_mat = new THREE.Matrix4();
         rot_mat.setRotationAxis(direction, PI * 2 / num_branches * i);
         rot_mat.rotateAxis(new_direction);
 
+        // random new length for the next branch
         new_length = (rand() * (MAX_LENGTH_FACTOR - MIN_LENGTH_FACTOR) + 
                      MIN_LENGTH_FACTOR) * length;
 
+        // caculate the position of the new branch
         new_position = new_base_position.clone();
         offset_vector = half_length_offset.clone();
         new_position.addSelf(
                 offset_vector.multiplyScalar(
                     2 * i / (num_branches - 1) * (1 - MIN_OFFSET_FACTOR)));
 
+        // using setTimeout to make the drawing procedure non-blocking
         setTimeout((function(a, b, c, d, e) {
             return function() {
                 drawTree(a, b, c, d, e);
             };
         })(new_position, new_direction, new_length, new_depth, new_radius), 0);
+
+        // more branches to draw, therefore increment the branch counter
+        branch_counter++;
     }
-
-    // recursively generate child-branches
-    /*
-    for (var i = 0; i < num_branches; ++i) {
-
-        new_direction = new THREE.Vector3(rand() - 0.5, 
-                                          rand() - 0.5, 
-                                          rand() - 0.5);
-        new_direction.normalize();
-        new_direction.addSelf(direction.clone().multiplyScalar(1.2));
-        new_direction.normalize();
-        
-        new_length = rand() % (MAX_LENGTH_FACTOR - MIN_LENGTH_FACTOR) + 
-                     MIN_LENGTH_FACTOR * length;
-
-        new_position = new_base_position.clone();
-        offset_vector = half_length_offset.clone();
-        new_position.addSelf(
-                offset_vector.multiplyScalar(
-                    2 * i / (num_branches - 1) * (1 - MIN_OFFSET_FACTOR)));
-
-        setTimeout((function(a, b, c, d, e) {
-            return function() {
-                drawTree(a, b, c, d, e);
-            };
-        })(new_position, new_direction, new_length, new_depth, new_radius), 0);
-    }
-    */
 }
 
 function setupLights() {
@@ -307,67 +296,63 @@ function drawGround() {
 
 
 function animate(update) {
-    requestAnimationFrame(animate);
+    // continue the animation if there are branches not drawn
+    if (branch_counter) {
+        requestAnimationFrame(animate);
+    }
     renderer.clear();
     renderer.render(scene, camera);
     stats.update();
 }
 
 
-function removeInfo() {
-    (function (ele) {
-        ele.parentNode.removeChild(ele)
-    })(document.getElementById("info"));
-}
 
-
-function drawLeaf(position, direction, scale) {
+//function drawLeaf(position, direction, scale) {
     
-    // leaf vertices
-    var vertices = [
-        [0, 0, 0],
-        [1, 0, -1],
-        [0, 0, 2],
-        [-1, 0, -1],
-    ];
-    vertices.forEach(function (v, i, a) {
-        a[i].forEach(function (vv, ii, aa) {
-            aa[ii] *= scale;
-        });
-    }); 
+    //// leaf vertices
+    //var vertices = [
+        //[0, 0, 0],
+        //[1, 0, -1],
+        //[0, 0, 2],
+        //[-1, 0, -1],
+    //];
+    //vertices.forEach(function (v, i, a) {
+        //a[i].forEach(function (vv, ii, aa) {
+            //aa[ii] *= scale;
+        //});
+    //}); 
 
-    // leaf geometry
-    var geometry = new THREE.Geometry();
-    geometry.vertices = vertices.map(function (v) {
-        return new THREE.Vertex(
-            new THREE.Vector3(v[0], v[1], v[2])
-        );
-    });
-    geometry.faces.push(new THREE.Face3(0, 1, 2));
-    geometry.faces.push(new THREE.Face3(0, 2, 3));
-    geometry.faces.push(new THREE.Face3(0, 2, 1));
-    geometry.faces.push(new THREE.Face3(0, 3, 2));
+    //// leaf geometry
+    //var geometry = new THREE.Geometry();
+    //geometry.vertices = vertices.map(function (v) {
+        //return new THREE.Vertex(
+            //new THREE.Vector3(v[0], v[1], v[2])
+        //);
+    //});
+    //geometry.faces.push(new THREE.Face3(0, 1, 2));
+    //geometry.faces.push(new THREE.Face3(0, 2, 3));
+    //geometry.faces.push(new THREE.Face3(0, 2, 1));
+    //geometry.faces.push(new THREE.Face3(0, 3, 2));
 
-    geometry.computeCentroids();
-    geometry.computeFaceNormals();
+    //geometry.computeCentroids();
+    //geometry.computeFaceNormals();
 
-    // leaf material
-    var material = new THREE.MeshLambertMaterial({
-        color: 0x00ff00,
-    });
+    //// leaf material
+    //var material = new THREE.MeshLambertMaterial({
+        //color: 0x00ff00,
+    //});
 
-    var leaf = new THREE.Mesh(geometry, material);
-    leaf.lookAt(direction);
-    leaf.position = position;
+    //var leaf = new THREE.Mesh(geometry, material);
+    //leaf.lookAt(direction);
+    //leaf.position = position;
 
-    scene.addObject(leaf);
-}
+    //scene.addObject(leaf);
+//}
 
 window.onload = function () {
 
     if (!Detector.webgl) {
         Detector.addGetWebGLMessage();
-        removeInfo();
         return;
     }
 
@@ -377,15 +362,17 @@ window.onload = function () {
 
     drawGround();
 
-    /*
-    drawCoordinate(new THREE.Vector3(0, 0, 0),  // center
-                   200);                        // length
+    //drawCoordinate(new THREE.Vector3(0, 0, 0),  // center
+                   //200);                        // length
     
 
-    drawGridPlane(new THREE.Vector3(0, 0, 0),  // center
-                  500,                         // length
-                  20);                         // num segs
-                  */
+    //drawGridPlane(new THREE.Vector3(0, 0, 0),  // center
+                  //500,                         // length
+                  //20);                         // num segs
+
+    // Set the initial branch counter to 1
+    // the animation will stop when the counter decreases to 0
+    branch_counter = 1;
 
     drawTree(new THREE.Vector3(0, 0, 0), // start position
              new THREE.Vector3(0, 1, 0), // direction
